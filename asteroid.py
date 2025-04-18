04b7865af0585995483779ad8a6013220435b6d3
Whythree <whythree1.dollars@gmail.com>
2025-04-18 17:54:00 +0200

    Spawning new Asteroids when hit by shot

--- asteroid.py
+++ asteroid.py
@@ -1,12 +1,31 @@
 from circleshape import CircleShape
 import pygame
+import random
+
+from constants import ASTEROID_MIN_RADIUS
+
 
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, velocity=pygame.Vector2(0,0)):
         super().__init__(x,y,radius)
+        self.velocity = velocity
 
     def draw(self, screen):
         pygame.draw.circle(screen,"white",self.position,self.radius,2)
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        random_angle = random.uniform(20,50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_spawn_velocity = self.velocity.rotate(random_angle)
+        asteroid_spawn_velocity_negative = self.velocity.rotate(-random_angle)
+
+        new_asteroid = Asteroid(self.position.x, self.position.y, new_radius, asteroid_spawn_velocity * 1.2)
+        new_asteroid = Asteroid(self.position.x, self.position.y, new_radius, asteroid_spawn_velocity_negative * 1.2)
--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
 
             for shot in shots:
                 if asteroid.collide(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
             if asteroid.collide(player):
